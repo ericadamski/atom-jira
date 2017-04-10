62f1adb46f131b3fb5180d0065a3987b075072c8
Eric Adamski <er.adamski@gmail.com>
2017-04-10 10:13:35 -0400

    Initial commit.

---
+++ spec/jira-issues-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JiraIssuesView from '../lib/jira-issues-view';
+
+describe('JiraIssuesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
